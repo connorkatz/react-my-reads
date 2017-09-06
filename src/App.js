68d8e97ea9e7cd2b4ecd0828de7f94b6ced7143f
Connor Katz <connorkatz@gmail.com>
2017-09-06 08:46:30 -0700

    Adding react-router

--- src/App.js
+++ src/App.js
@@ -1,61 +1,21 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import BookLibrary from './BookLibrary';
+import './App.css';
 
-class BooksApp extends React.Component {
-   state = {
-      /**
-       * TODO: Instead of using this state variable to keep track of which page
-       * we're on, use the URL in the browser's address bar. This will ensure that
-       * users can use the browser's back and forward buttons to navigate between
-       * pages, as well as provide a good URL they can bookmark and share.
-       */
-      showSearchPage: false
-   }
-
+class App extends React.Component {
    render() {
       return (
-         <div className="app">
-            <div className="list-books">
-               <div className="list-books-title">
-                  <h1>MyReads</h1>
-               </div>
-               <div className="list-books-content">
-                  <div>
-                     <div className="bookshelf">
-                        <h2 className="bookshelf-title">Currently Reading</h2>
-                        <div className="bookshelf-books">
-                           <ol className="books-grid">
-                              <li>
-                                 <div className="book">
-                                    <div className="book-top">
-                                       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                                       <div className="book-shelf-changer">
-                                          <select>
-                                             <option value="none" disabled>Move to...</option>
-                                             <option value="currentlyReading">Currently Reading</option>
-                                             <option value="wantToRead">Want to Read</option>
-                                             <option value="read">Read</option>
-                                             <option value="none">None</option>
-                                          </select>
-                                       </div>
-                                    </div>
-                                    <div className="book-title">To Kill a Mockingbird</div>
-                                    <div className="book-authors">Harper Lee</div>
-                                 </div>
-                              </li>
-                           </ol>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div className="open-search">
-                  <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-               </div>
-            </div>
+         <div className="my-reads-app">
+            <Route
+               exact path="/"
+               render={() => (<BookLibrary/>)}
+            />
+            
          </div>
       )
    }
 }
 
-export default BooksApp
+export default App;
---
+++ src/BookLibrary.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BookLibrary extends Component {
+   render() {
+      return (
+         <div>
+            The Book Library
+         </div>
+      )
+   }
+}
+
+export default BookLibrary;
+
